97d4b63d655bde033fc91eda78b25810e7c74b55
Gaok1 <luisphilliplemosmartins@yahoo.com.br>
2023-09-22 19:20:06 -0300

    forum

---
+++ MavenProject/inicio/src/main/java/Estruturas/Objetos/Forum.java
@@ -0,0 +1,46 @@
+package Estruturas.Objetos;
+
+public class Forum {
+    int GameID;
+    String Nome;
+    int id;
+
+    public Forum(int GameID, String Nome, int id) { // lembre que o id do forum é serial, não precisa inseri-lo no bd
+        this.GameID = GameID;
+        this.Nome = Nome;
+        this.id = id;
+    }
+
+    public Forum(int GameID, String Nome) { // lembre que o id do forum é serial, não precisa inseri-lo no bd
+        this.GameID = GameID;
+        this.Nome = Nome;
+    }
+
+    // getter methods
+    public int getGameID() {
+        return GameID;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    //setters
+    public void setGameID(int GameID) {
+        this.GameID = GameID;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+}
