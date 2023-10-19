262556ca33e7b6ef650537dd56004d725cb64eb4
Gaok1 <luisphilliplemosmartins@yahoo.com.br>
2023-10-19 11:10:49 -0300

    Update Script.sql

--- MavenProject/inicio/src/main/resources/Script.sql
+++ MavenProject/inicio/src/main/resources/Script.sql
@@ -1,66 +1,58 @@
-Create table games(
-appid (int) priumary key,
-json (text),
-categoria (int),
-CONSTRAINT fk_games_categoria FOREIGN KEY (categoria) REFERENCES categoria(id)
-)
+CREATE TABLE categoria (
+    id serial PRIMARY KEY,
+    nome VARCHAR(45) NOT NULL
+);
 
+
+CREATE TABLE games (
+    appid serial PRIMARY KEY,
+    json text,
+    categoria integer,
+    CONSTRAINT fk_games_categoria FOREIGN KEY (categoria) REFERENCES categoria(id)
+);
 
 CREATE TABLE usuario (
-  ID SERIAL PRIMARY KEY,
-  Nome VARCHAR(46) NOT NULL,
-  Senha VARCHAR(20) NOT NULL,
-  Email VARCHAR(46) NOT NULL,
-  Foto BYTEA NOT NULL, -- BYTEA é o tipo adequado para armazenar dados binários, como imagens
-  DataNasc DATE NOT NULL,
-  DataCadastro DATE NOT NULL
+    ID serial PRIMARY KEY,
+    Nome VARCHAR(46) NOT NULL,
+    Senha VARCHAR(20) NOT NULL,
+    Email VARCHAR(46) NOT NULL,
+    Foto BYTEA NOT NULL,
+    DataNasc DATE NOT NULL,
+    DataCadastro DATE NOT NULL
 );
 
-
-CREATE TABLE Forum (
- id  serial ,
-	nome varchar(45) not null,
-	jogos_appid int not null,
-	primary key (id,jogos_appid),
-	constraint fk_forum_jogosAppid foreign key (jogos_appid) references games (appid)
-	
+CREATE TABLE forum (
+    id serial PRIMARY KEY,
+    nome varchar(45) NOT NULL,
+    jogos_appid int NOT NULL,
+    CONSTRAINT fk_forum_jogosAppid FOREIGN KEY (jogos_appid) REFERENCES games (appid)
 );
-
 
 CREATE TABLE Posts (
-  id serial,
-  Postagem TEXT NOT NULL,
-  Forum_ID INT NOT NULL,
-  User_ID INT NOT NULL,
-  categoria_ID INT NOT NULL,
-  PRIMARY KEY (id),
-  CONSTRAINT fk_Post_ForumID FOREIGN KEY (Forum_ID)  REFERENCES forum (id),
-  CONSTRAINT fk_Post_UserID FOREIGN KEY (User_ID) REFERENCES usuario (id),
-  CONSTRAINT fk_Post_CategoriaID FOREIGN KEY (categoria_ID) REFERENCES categoria (id)
-
+    id serial PRIMARY KEY,
+    Postagem TEXT NOT NULL,
+    Forum_ID INT NOT NULL,
+    User_ID INT NOT NULL,
+    categoria_ID INT NOT NULL,
+    CONSTRAINT fk_Post_ForumID FOREIGN KEY (Forum_ID) REFERENCES forum (id),
+    CONSTRAINT fk_Post_UserID FOREIGN KEY (User_ID) REFERENCES usuario (id),
+    CONSTRAINT fk_Post_CategoriaID FOREIGN KEY (categoria_ID) REFERENCES categoria (id)
 );
-
 
 CREATE TABLE comentario (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  coment TEXT NOT NULL,
-  DataPostagem DATETIME,
-  PostID INT NOT NULL,
-  UserID INT NOT NULL,
-  CONSTRAINT fk_Coment_postId FOREIGN KEY (PostID) REFERENCES Posts(ID),
-  CONSTRAINT fk_Coment_userId FOREIGN KEY (UserID) REFERENCES User(ID)
+    id serial PRIMARY KEY,
+    coment TEXT NOT NULL,
+    DataPostagem TIMESTAMPTZ,
+    PostID INT NOT NULL,
+    UserID INT NOT NULL,
+    CONSTRAINT fk_Coment_postId FOREIGN KEY (PostID) REFERENCES Posts(id),
+    CONSTRAINT fk_Coment_userId FOREIGN KEY (UserID) REFERENCES usuario (ID)
 );
 
-CREATE TABLE categoria{
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-nome VARCHAR(45) NOT NULL
-}
 
-CREATE TABLE subcoment{
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  conteudo text NOT NULL,
-  comentID INT NOT NULL,
-  CONSTRAINT fk_subcoment_ComentID FOREIGN KEY(comentID) REFERENCE comentario(ID)
-}
-
-
+CREATE TABLE subcoment (
+    id serial PRIMARY KEY,
+    conteudo text NOT NULL,
+    comentID INT NOT NULL,
+    CONSTRAINT fk_subcoment_ComentID FOREIGN KEY (comentID) REFERENCES comentario(id)
+);
